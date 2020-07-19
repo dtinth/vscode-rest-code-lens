e11bb043cbf8d540ecc7ea89f4706a94be214ac1
Thai Pangsakulyanont <dtinth@spacet.me>
2020-07-19 18:49:49 +0700

    Here is some work in progress lol

---
+++ src/extension.ts
@@ -0,0 +1,154 @@
+import { setTimeout } from 'timers'
+import * as vscode from 'vscode'
+
+type Provider = {
+  pattern: string
+  url: string
+  flags?: string
+}
+
+function ensureGlobal(flags: string) {
+  return flags + (flags.includes('g') ? '' : 'g')
+}
+
+type RestLensMatch = {
+  range: vscode.Range
+  providerId: string
+  requestUrl: string
+}
+
+type RestLensMatchCache = {
+  lastVersion: number
+  matches: RestLensMatch[]
+}
+
+type RestLensRequestCache = {
+  command: vscode.Command
+  expires?: number
+}
+
+class RestCodeLensProvider implements vscode.CodeLensProvider<vscode.CodeLens> {
+  private emitter = new vscode.EventEmitter<void>()
+  private matchCache = new WeakMap<vscode.TextDocument, RestLensMatchCache>()
+  private requestCache = new Map<string, RestLensRequestCache>()
+  private gonnaUpdate = false
+
+  onDidChangeCodeLenses = this.emitter.event
+
+  provideCodeLenses(document: vscode.TextDocument) {
+    let matchCache = this.matchCache.get(document)
+
+    if (!matchCache) {
+      matchCache = { lastVersion: -1, matches: [] }
+      this.matchCache.set(document, matchCache)
+    }
+
+    if (document.version > matchCache.lastVersion) {
+      matchCache.lastVersion = document.version
+      matchCache.matches = []
+
+      const providers = vscode.workspace
+        .getConfiguration('restLens', document)
+        .get<{ [key: string]: Provider }>('providers', {})
+
+      for (const [providerId, provider] of Object.entries(providers)) {
+        try {
+          if (provider) {
+            const pattern = String(provider.pattern)
+            const flags = ensureGlobal(provider.flags || '')
+            const url = String(provider.url)
+            const documentMatches = document
+              .getText()
+              .matchAll(new RegExp(pattern, flags))
+            for (const match of documentMatches) {
+              if (match.index == null) continue
+              if (match.input == null) continue
+              const range = new vscode.Range(
+                document.positionAt(match.index),
+                document.positionAt(match.index + match.input.length),
+              )
+              const captureGroups = match.slice()
+              const requestUrl =
+                url +
+                (url.includes('?') ? '&' : '?') +
+                captureGroups
+                  .map((g) => 'm[]=' + encodeURIComponent(g))
+                  .join('&')
+              matchCache.matches.push({ range, requestUrl, providerId })
+            }
+          }
+        } catch (error) {
+          console.error(providerId, error)
+        }
+      }
+    }
+
+    return matchCache.matches.map((m) => this.getCodeLens(m))
+  }
+
+  private getCodeLens(matchItem: RestLensMatch): vscode.CodeLens {
+    const key = matchItem.providerId + ':' + matchItem.requestUrl
+    let lens = this.requestCache.get(key)
+    if (!lens || (lens.expires && Date.now() > lens.expires)) {
+      lens = {
+        command: {
+          title: `(${matchItem.providerId})`,
+          command: 'restLens.open',
+          arguments: [],
+        },
+      }
+      this.requestCache.set(key, lens)
+      ;(async () => {
+        try {
+          await new Promise((resolve) => setTimeout(resolve, 3000))
+          lens.command = {
+            title: `(resolved => ${matchItem.requestUrl})`,
+            command: 'restLens.open',
+            arguments: [],
+          }
+        } catch (error) {
+          lens.command = {
+            title: `(error => ${matchItem.providerId})`,
+            command: 'restLens.open',
+            arguments: [],
+          }
+        } finally {
+          if (!this.gonnaUpdate) {
+            this.gonnaUpdate = true
+            setTimeout(() => {
+              this.gonnaUpdate = false
+              this.emitter.fire()
+            }, 100)
+          }
+        }
+      })()
+    }
+    return {
+      isResolved: true,
+      command: lens.command,
+      range: matchItem.range,
+    }
+  }
+}
+
+export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.languages.registerCodeLensProvider(
+      [
+        { scheme: 'file' },
+        { scheme: 'git' },
+        { scheme: 'vsls' },
+        { scheme: 'untitled' },
+      ],
+      new RestCodeLensProvider(),
+    ),
+  )
+  context.subscriptions.push(
+    vscode.commands.registerCommand('restLens.open', (arg) => {
+      vscode.window.showInformationMessage('Hello World from rest-lens! ' + arg)
+    }),
+  )
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
